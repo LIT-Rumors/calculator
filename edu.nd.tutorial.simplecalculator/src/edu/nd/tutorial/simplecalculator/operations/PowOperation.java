01b42a8e67a6708623b3405fdd0175c4e3ec7dc6
github@vierhauser.net <github@vierhauser.net>
2017-05-19 16:18:34 -0400

    Pow-Operation added

---
+++ edu.nd.tutorial.simplecalculator/src/edu/nd/tutorial/simplecalculator/operations/PowOperation.java
@@ -0,0 +1,19 @@
+package edu.nd.tutorial.simplecalculator.operations;
+
+import edu.nd.tutorial.simplecalculator.ui.ICalculationOperation;
+
+public class PowOperation implements ICalculationOperation {
+
+	
+		@Override
+		public String caclulate(String txt) {
+			System.out.println("Power Operation Executed");
+			String[] terms = txt.split("\\^");
+			if (terms.length == 2) {
+				return Math.pow(Integer.parseInt(terms[0]), Integer.parseInt(terms[1]))+"";
+			}
+			return txt;
+
+		}
+
+}
